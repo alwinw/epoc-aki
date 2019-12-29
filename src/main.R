a113cf295e1cbd027555850dd899017517eb975f
AlwinW <16846521+AlwinW@users.noreply.github.com>
2019-12-29 22:07:20 +1100

    Screened out collection errors
    Based on UR number, Dates_screened and Excl_criteria_ok

---
+++ src/main.R
@@ -0,0 +1,9 @@
+# main entry point
+# Alwin Wang 2019
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("library-manager.R")
+
+oliguria_xlsx_path     = file.path("../data" ,"Creatinine change in oliguria 27.9.18.xlsx")
+creatinine_xlsx_path   = file.path("../data" ,"Small changes in creatinine 27.9.18.xlsx")
+demographics_xlsx_path = file.path("../data" ,"Demographics pts screened out.xlsx")
