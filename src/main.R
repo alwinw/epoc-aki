63289adb3155480b04fa0f3bedc6bdf260af40b8
AlwinW <16846521+AlwinW@users.noreply.github.com>
2019-12-29 22:27:48 +1100

    Determined excluded UR and Pt study numbers
    Next step is to merge creatinine and oliguria together

--- src/main.R
+++ src/main.R
@@ -7,3 +7,5 @@ source("library-manager.R")
 oliguria_xlsx_path     = file.path("../data" ,"Creatinine change in oliguria 27.9.18.xlsx")
 creatinine_xlsx_path   = file.path("../data" ,"Small changes in creatinine 27.9.18.xlsx")
 demographics_xlsx_path = file.path("../data" ,"Demographics pts screened out.xlsx")
+
+source("merge-excel.R")
