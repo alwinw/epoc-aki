0fe34e53a0f4a7ad208f05e79811bec7d2f2390c
AlwinW <16846521+AlwinW@users.noreply.github.com>
2019-12-31 18:54:28 +1100

    Applied data checks
    Will now stop and throw error if UR discards are inconsistent

--- src/merge-excel.R
+++ src/merge-excel.R
@@ -1,31 +1,36 @@
 # Merge Excel Files
 # Alwin Wang 2019
 
+suppressMessages(
 oliguria_xlsx_data <- list(
   demographic = read_excel(oliguria_xlsx_path, "Patient Demographics"),
   data_set    = read_excel(oliguria_xlsx_path, "Data set"            ),
   outcomes    = read_excel(oliguria_xlsx_path, "AKI & outcomes"      ),
   screen_log  = read_excel(oliguria_xlsx_path, "Screening log"       )
-)
+))
+suppressMessages(
 creatinine_xlsx_data <- list(
   demographic = read_excel(creatinine_xlsx_path, "Patient Demographics"),
   data_set    = read_excel(creatinine_xlsx_path, "Data set"            ),
   outcomes    = read_excel(creatinine_xlsx_path, "AKI & outcomes"      ),
   screen_log  = read_excel(creatinine_xlsx_path, "Screening log"       )
-)
+))
+suppressMessages(
 demographics_xlsx_data <- list(
   num_creatinine = read_excel(demographics_xlsx_path, "no cr change"       ),
   num_oliguria   = read_excel(demographics_xlsx_path, "no oliguria"        ),
   neither_cr_ol  = read_excel(demographics_xlsx_path, "neither cr nor olig")
-)
+))
 
 #
 
 excel_date_to_character <- function(vector) {
+  suppressWarnings(
   ifelse(
     grepl("/", vector), 
     vector, 
     as.character(as.Date(as.numeric(vector), origin = "1899-12-30"), format = "%d/%m/%y"))
+  )
 }
 
 oliguria_xlsx_data$screen_log   %<>% arrange(`UR number`, Dates_screened) %>% 
@@ -51,4 +56,32 @@ cat(paste(
   "Discarded UR Numbers:       ", paste(excluded_UR_numbers , collapse = ", "), "\n",
   "Discarded Pt Study Numbers: ", paste(excluded_Pt_Study_no, collapse = ", "), "\n"
 ))
+
+#
+
+merge_columns = setdiff(
+  intersect(colnames(creatinine_xlsx_data$screen_log), colnames(oliguria_xlsx_data$screen_log)),
+  c("Incl_criteria_ok", "Pt_Study_no", tail(colnames(creatinine_xlsx_data$screen_log),1))
+)
+merged_xlsx_data = list(screen_log = 
+    full_join(
+      filter(creatinine_xlsx_data$screen_log, !(`UR number` %in% excluded_UR_numbers)),
+      filter(oliguria_xlsx_data  $screen_log, !(`UR number` %in% excluded_UR_numbers)),
+      by     = merge_columns,
+      suffix = c("_cre", "_oli")
+))
+if (anyNA(merged_xlsx_data$screen_log$`UR number`)) {
+  stop("NA in UR number of merged Excel sheets")
+}
+creatinine_xlsx_n_obs = nrow(filter(creatinine_xlsx_data$screen_log, !(`UR number` %in% excluded_UR_numbers)))
+oliguria_xlsx_n_obs   = nrow(filter(oliguria_xlsx_data$screen_log  , !(`UR number` %in% excluded_UR_numbers)))
+merged_xlsx_n_obs     = nrow(merged_xlsx_data$screen_log)
+if (!all(sapply(list(creatinine_xlsx_n_obs, oliguria_xlsx_n_obs),
+    function(x) x == merged_xlsx_n_obs))) {
+  stop(paste0(
+    "Inconsistent number of n_obs after UR numbers discarded. ",
+    "Creatinine: ", creatinine_xlsx_n_obs, ", ",
+    "Oliguria: "  , oliguria_xlsx_n_obs,   ", ",
+    "Merged: "    , merged_xlsx_n_obs,     ", "))
+}
 
