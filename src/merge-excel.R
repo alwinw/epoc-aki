4f9a06a41c659e1a6ed3b14ca562dcba5191c264
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-01 20:55:18 +1100

    Functionalised merge
    To do: merge all data

--- src/merge-excel.R
+++ src/merge-excel.R
@@ -2,26 +2,42 @@
 # Alwin Wang 2019
 
 # Read Excel Files
-suppressMessages(
-oliguria_xlsx_data <- list(
-  demographic = read_excel(oliguria_xlsx_path, "Patient Demographics"),
-  data_set    = read_excel(oliguria_xlsx_path, "Data set"            ),
-  outcomes    = read_excel(oliguria_xlsx_path, "AKI & outcomes"      ),
-  screen_log  = read_excel(oliguria_xlsx_path, "Screening log"       )
-))
-suppressMessages(
-creatinine_xlsx_data <- list(
-  demographic = read_excel(creatinine_xlsx_path, "Patient Demographics"),
-  data_set    = read_excel(creatinine_xlsx_path, "Data set"            ),
-  outcomes    = read_excel(creatinine_xlsx_path, "AKI & outcomes"      ),
-  screen_log  = read_excel(creatinine_xlsx_path, "Screening log"       )
-))
-suppressMessages(
-demographics_xlsx_data <- list(
-  num_creatinine = read_excel(demographics_xlsx_path, "no cr change"       ),
-  num_oliguria   = read_excel(demographics_xlsx_path, "no oliguria"        ),
-  neither_cr_ol  = read_excel(demographics_xlsx_path, "neither cr nor olig")
-))
+load_excel_files <- function() {
+  oliguria_xlsx <- list(
+    demographic = read_excel(oliguria_xlsx_path, "Patient Demographics"),
+    data_set    = read_excel(oliguria_xlsx_path, "Data set"            ),
+    outcomes    = read_excel(oliguria_xlsx_path, "AKI & outcomes"      ),
+    screen_log  = read_excel(oliguria_xlsx_path, "Screening log"       )
+  )
+  creatinine_xlsx <- list(
+    demographic = read_excel(creatinine_xlsx_path, "Patient Demographics"),
+    data_set    = read_excel(creatinine_xlsx_path, "Data set"            ),
+    outcomes    = read_excel(creatinine_xlsx_path, "AKI & outcomes"      ),
+    screen_log  = read_excel(creatinine_xlsx_path, "Screening log"       )
+  )
+  demographics_xlsx <- list(
+    num_creatinine = read_excel(demographics_xlsx_path, "no cr change"       ),
+    num_oliguria   = read_excel(demographics_xlsx_path, "no oliguria"        ),
+    neither_cr_ol  = read_excel(demographics_xlsx_path, "neither cr nor olig")
+  )
+  creat_furo_xlsx <- list(
+    blood_gas    = read_excel(creat_furo_xlsx_path, "Blood Gas"              ),
+    bio_chem     = read_excel(creat_furo_xlsx_path, "BioChem"                ),
+    lowest_creat = read_excel(creat_furo_xlsx_path, "Lowest Creatinine Level"),
+    furosemide   = read_excel(creat_furo_xlsx_path, "Medication"             )
+  )
+  
+  xlsx_data <- list(
+    oliguria     = oliguria_xlsx    ,
+    creatinine   = creatinine_xlsx  ,
+    demographics = demographics_xlsx,
+    time_series  = creat_furo_xlsx
+  )
+  return(xlsx_data)
+}
+
+xlsx_data <- suppressMessages(load_excel_files())
+str(xlsx_data)
 
 # Convert Excel Dates
 excel_date_to_character <- function(vector) {
@@ -33,47 +49,64 @@ excel_date_to_character <- function(vector) {
   )
 }
 
-oliguria_xlsx_data$screen_log   %<>% arrange(`UR number`, Dates_screened) %>% 
-  mutate(Dates_screened = excel_date_to_character(Dates_screened))
+convert_excel <- function(xlsx_data) {
+  xlsx_data$oliguria$screen_log   %<>% arrange(`UR number`, Dates_screened) %>% 
+    mutate(Dates_screened = excel_date_to_character(Dates_screened))
+  xlsx_data$creatinine$screen_log %<>% arrange(`UR number`, Dates_screened) %>% 
+    mutate(Dates_screened = excel_date_to_character(Dates_screened))
+  return(xlsx_data)
+}
 
-creatinine_xlsx_data$screen_log %<>% arrange(`UR number`, Dates_screened) %>% 
-  mutate(Dates_screened = excel_date_to_character(Dates_screened))
+xlsx_data <- convert_excel(xlsx_data)
 
 # Data collection errors
-data_collection_errors = 
-  (creatinine_xlsx_data$screen_log$`UR number`      != oliguria_xlsx_data$screen_log$`UR number`     ) |
-  (creatinine_xlsx_data$screen_log$Dates_screened   != oliguria_xlsx_data$screen_log$Dates_screened  ) |
-  (creatinine_xlsx_data$screen_log$Excl_criteria_ok != oliguria_xlsx_data$screen_log$Excl_criteria_ok)
-
-creatinine_collection_errors = creatinine_xlsx_data$screen_log[data_collection_errors,]
-oliguria_collection_errors   = oliguria_xlsx_data  $screen_log[data_collection_errors,]
-
-excluded_UR_numbers  =   creatinine_collection_errors$`UR number`
-excluded_Pt_Study_no = discard(c(oliguria_collection_errors$Pt_Study_no, creatinine_collection_errors$Pt_Study_no), is.na)
-cat(paste(
-  "\n",
-  "Discarded UR Numbers:       ", paste(excluded_UR_numbers , collapse = ", "), "\n",
-  "Discarded Pt Study Numbers: ", paste(excluded_Pt_Study_no, collapse = ", "), "\n"
-))
+data_collection_errors <- function(xlsx_data) {
+  errors_logi = 
+    (xlsx_data$creatinine$screen_log$`UR number`      != xlsx_data$oliguria$screen_log$`UR number`     ) |
+    (xlsx_data$creatinine$screen_log$Dates_screened   != xlsx_data$oliguria$screen_log$Dates_screened  ) |
+    (xlsx_data$creatinine$screen_log$Excl_criteria_ok != xlsx_data$oliguria$screen_log$Excl_criteria_ok)
+  creatinine_errors = xlsx_data$creatinine$screen_log[errors_logi, ]
+  oliguria_errors   = xlsx_data$oliguria  $screen_log[errors_logi, ]
+  
+  xlsx_data$excluded_UR_numbers  = creatinine_errors$`UR number`
+  xlsx_data$excluded_Pt_Study_no = discard(c(oliguria_errors$Pt_Study_no, creatinine_errors$Pt_Study_no), is.na)
+  
+  cat("\nCreatinine xlsx sheet\n")
+  print(creatinine_errors[, c(13, 2:4)])
+  cat("\nOliguria xlsx sheet\n"  )
+  print(oliguria_errors  [, c(13, 2:4)])
+  
+  cat(paste("\n",
+    # "Discarded UR Numbers:       ", paste(xlsx_data$excluded_UR_numbers , collapse = ", "), "\n",
+    "Discarded Pt Study Numbers: ", paste(xlsx_data$excluded_Pt_Study_no, collapse = ", "), "\n"
+  ))
+  
+  xlsx_data$discarded_pts <- creatinine_errors %>% 
+    mutate(
+      Excl_criteria_ok = "N",
+      Already_AKI      = "Y")
+  
+  return(xlsx_data)
+}
 
 # Merge screening logs
 merge_columns = setdiff(
-  intersect(colnames(creatinine_xlsx_data$screen_log), colnames(oliguria_xlsx_data$screen_log)),
-  c("Incl_criteria_ok", "Pt_Study_no", tail(colnames(creatinine_xlsx_data$screen_log),1))
+  intersect(colnames(creatinine_xlsx$screen_log), colnames(oliguria_xlsx$screen_log)),
+  c("Incl_criteria_ok", "Pt_Study_no", tail(colnames(creatinine_xlsx$screen_log),1))
 )
-merged_xlsx_data = list(screen_log = 
+merged_xlsx = list(screen_log = 
     full_join(
-      filter(creatinine_xlsx_data$screen_log, !(`UR number` %in% excluded_UR_numbers)),
-      filter(oliguria_xlsx_data  $screen_log, !(`UR number` %in% excluded_UR_numbers)),
+      filter(creatinine_xlsx$screen_log, !(`UR number` %in% excluded_UR_numbers)),
+      filter(oliguria_xlsx  $screen_log, !(`UR number` %in% excluded_UR_numbers)),
       by     = merge_columns,
       suffix = c("_cre", "_oli")
 ))
-if (anyNA(merged_xlsx_data$screen_log$`UR number`)) {
+if (anyNA(merged_xlsx$screen_log$`UR number`)) {
   stop("NA in UR number of merged Excel sheets")
 }
-creatinine_xlsx_n_obs = nrow(filter(creatinine_xlsx_data$screen_log, !(`UR number` %in% excluded_UR_numbers)))
-oliguria_xlsx_n_obs   = nrow(filter(oliguria_xlsx_data$screen_log  , !(`UR number` %in% excluded_UR_numbers)))
-merged_xlsx_n_obs     = nrow(merged_xlsx_data$screen_log)
+creatinine_xlsx_n_obs = nrow(filter(creatinine_xlsx$screen_log, !(`UR number` %in% excluded_UR_numbers)))
+oliguria_xlsx_n_obs   = nrow(filter(oliguria_xlsx$screen_log  , !(`UR number` %in% excluded_UR_numbers)))
+merged_xlsx_n_obs     = nrow(merged_xlsx$screen_log)
 if (length(unique(c(creatinine_xlsx_n_obs, oliguria_xlsx_n_obs, merged_xlsx_n_obs))) != 1) {
   stop(paste0(
     "Inconsistent number of n_obs after UR numbers discarded. ",
@@ -84,16 +117,16 @@ if (length(unique(c(creatinine_xlsx_n_obs, oliguria_xlsx_n_obs, merged_xlsx_n_ob
 
 #
 
-merged_xlsx_colnames = colnames(merged_xlsx_data$screen_log)
-merged_xlsx_log_colnames = merged_xlsx_colnames[
+merged_xlsx_colnames = colnames(merged_xlsx$screen_log)
+merged_xlsx_logi_colnames = merged_xlsx_colnames[
   !(merged_xlsx_colnames %in%
     grep(paste0("UR number|Dates_screened|Pt_Study_no|Total_no_|", 
-                tail(colnames(creatinine_xlsx_data$screen_log),1)), 
+                tail(colnames(creatinine_xlsx$screen_log),1)), 
        merged_xlsx_colnames, value = TRUE))
 ]
-merged_xlsx_data$screen_log[merged_xlsx_log_colnames] <- 
-  ifelse(merged_xlsx_data$screen_log[merged_xlsx_log_colnames] == "N" | 
-         is.na(merged_xlsx_data$screen_log[merged_xlsx_log_colnames]), 
+merged_xlsx$screen_log[merged_xlsx_logi_colnames] <- 
+  ifelse(merged_xlsx$screen_log[merged_xlsx_logi_colnames] == "N" | 
+         is.na(merged_xlsx$screen_log[merged_xlsx_logi_colnames]), 
        FALSE, 
        TRUE)
 
@@ -104,5 +137,5 @@ DateTime <- function(date, time) {
   else return(as.POSIXct(paste(date, format(time, format = "%H:%M:%S"))))
 }
 
-merged_xlsx_data$creatinine <- creatinine_xlsx_data$data_set %>% 
+merged_xlsx$creatinine <- creatinine_xlsx$data_set %>% 
   fill(Pt_Study_no)
