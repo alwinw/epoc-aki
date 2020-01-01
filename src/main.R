ca3080fd8b5f33605602cf79e6d2d33f3ae22d87
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-01 20:55:18 +1100

    Functionalised merge
    To do: merge all data

--- src/main.R
+++ src/main.R
@@ -4,8 +4,9 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("library-manager.R")
 
-oliguria_xlsx_path     = file.path("../data" ,"Creatinine change in oliguria 27.9.18.xlsx")
-creatinine_xlsx_path   = file.path("../data" ,"Small changes in creatinine 27.9.18.xlsx")
-demographics_xlsx_path = file.path("../data" ,"Demographics pts screened out.xlsx")
+oliguria_xlsx_path     = file.path("../data", "Creatinine change in oliguria 27.9.18.xlsx")
+creatinine_xlsx_path   = file.path("../data", "Small changes in creatinine 27.9.18.xlsx"  )
+demographics_xlsx_path = file.path("../data", "Demographics pts screened out.xlsx"        )
+creat_furo_xlsx_path   = file.path("../data", "ED_ICU_Creatinine_Furosemide.xlsx"         )
 
 source("merge-excel.R")
