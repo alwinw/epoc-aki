989ec9b864afaa345e6bcbd5cdfbbaa2f246f5d0
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-20 09:02:41 +1100

    Manually generated flow chart
    Consider going back to cat

--- R/003-flow-chart-screening.R
+++ R/003-flow-chart-screening.R
@@ -7,55 +7,38 @@ screening_data_flow_chart <- function(screening_data) {
     summarise(
       `Total Unique UR Numbers`     = length(unique(`UR number`)),
       `Total Admissions`            = n(),
-      `> Total Excluded Admissions` = length(`UR number`[!Excl_criteria_ok]),
-      `> Total Eligible Admissions` = length(`UR number`[ Excl_criteria_ok])
+      `|-- Total Excluded Admissions` = length(`UR number`[!Excl_criteria_ok]),
+      `°-- Total Eligible Admissions` = length(`UR number`[ Excl_criteria_ok])
     )
   
   flow_chart_excluded <- screening_data %>% 
     filter(!Excl_criteria_ok) %>% 
     summarise(
-      `| Total Excluded Admissions` = n(),
-      `|-- AKI`                = length(`UR number`[Already_AKI      ]),
-      `|-- Weekend`            = length(`UR number`[Admit_weekend    ]),
-      `|-- No ICD`             = length(`UR number`[No_IDC           ]),
-      `|-- ESKD`               = length(`UR number`[ESKD             ]),
-      `|-- EOLC`               = length(`UR number`[EOLC             ]),
-      `|-- Kidney transplant`  = length(`UR number`[Kidney_transplant]),
-      `°-- Child`              = length(`UR number`[Child            ])
+      `Total Excluded Admissions` = n(),
+      `|-- AKI`                = length(na.omit(`UR number`[Already_AKI      ])),
+      `|-- Weekend`            = length(na.omit(`UR number`[Admit_weekend    ])),
+      `|-- No ICD`             = length(na.omit(`UR number`[No_IDC           ])),
+      `|-- ESKD`               = length(na.omit(`UR number`[ESKD             ])),
+      `|-- EOLC`               = length(na.omit(`UR number`[EOLC             ])),
+      `|-- Kidney transplant`  = length(na.omit(`UR number`[Kidney_transplant])),
+      `°-- Child`              = length(na.omit(`UR number`[Child            ]))
     )
+  
+  flow_chart_included_cr <- screening_data %>% 
+    filter(Excl_criteria_ok) %>% 
+    group_by(Total_no_cr_epis) %>% 
+    summarise(
+      `> Total Eligible Admissions` = n()
+    )
+  
+  flow_chart_included_olig <- screening_data %>% 
+    filter(Excl_criteria_ok) %>% 
+    group_by(Total_no_olig_epis) %>% 
+    summarise(
+      `> Total Eligible Admissions` = n()
+    )
+  
+  flow_chart_included_both <- screening_data %>% 
+    filter(Excl_criteria_ok) 
+  # group by interaction between is.na(Epis_cr_change) and olig
 }
-
-
-admissions_flow_chart_excluded <- merged_xlsx_data$screen_log %>% 
-  filter(!Excl_criteria_ok) %>% 
-  summarise(
-    `| Total Excluded Admissions` = n(),
-    `|-- AKI`                     = length(`UR number`[Already_AKI      ]),
-    `|-- Weekend`                 = length(`UR number`[Admit_weekend    ]),
-    `|-- No ICD`                  = length(`UR number`[No_IDC           ]),
-    `|-- ESKD`                    = length(`UR number`[ESKD             ]),
-    `|-- EOLC`                    = length(`UR number`[EOLC             ]),
-    `|-- Kidney transplant`       = length(`UR number`[Kidney_transplant]),
-    `°-- Child`                   = length(`UR number`[Child            ])
-  ) # could do something more fancy here with gather?
-
-admissions_flow_chart_included_cr <- merged_xlsx_data$screen_log %>% 
-  filter(Excl_criteria_ok) %>% 
-  group_by(Total_no_cr_epis) %>% 
-  summarise(
-    `> Total Eligible Admissions` = n()
-  )
-
-admissions_flow_chart_included_olig <- merged_xlsx_data$screen_log %>% 
-  filter(Excl_criteria_ok) %>% 
-  group_by(Total_no_olig_epis) %>% 
-  summarise(
-    `> Total Eligible Admissions` = n()
-  )
-
-admissions_flow_chart_included_all <- merged_xlsx_data$screen_log %>% 
-  filter(Excl_criteria_ok) %>% 
-  group_by(Total_no_olig_epis) %>% 
-  summarise(
-    n = n()
-  )
