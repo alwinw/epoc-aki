da788629b73fd5359b27b9d3274c3c5203704313
Alwin <16846521+AlwinW@users.noreply.github.com>
2020-02-03 20:52:30 +1100

    Edited main scripts to reflect name change

--- index.R
+++ index.R
@@ -6,8 +6,7 @@ source("R/000-data-source.R")
 source("R/001-library-manager.R")
 source("R/002-merge-screening.R")
 
-
-source("R/005-merge-collection.R")
+source("R/004-merge-collection.R")
 
 # Run ----
 invisible(suppressMessages(load_library()))
@@ -16,5 +15,4 @@ xlsx_data <- suppressMessages(load_excel_files())
 xlsx_data <- convert_excel(xlsx_data)
 xlsx_data <- data_collection_errors(xlsx_data)
 screening_data <- merge_xlsx_screening(xlsx_data)
-
 analysis_data <- merge_xlsx_creatinine_oliguria(screening_data, xlsx_data)
