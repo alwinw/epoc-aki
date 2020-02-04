da22b60703117e1c5523d589aaee529ab57f20b6
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-02-05 09:38:48 +1100

    Cleaned up library manager
    Now reads in R-requirements.txt

--- R/001-library-manager.R
+++ R/001-library-manager.R
@@ -1,29 +1,30 @@
-# Library Manager
-# Alwin Wang 2019
+#' Library manager
+#'
+#' Loads packages from a specified requirements file and optionally writes
+#' them out to a references file
+#'
+#' @param req_file file containing the list of packages to load
+#' @param bib_file if not NULL, will write out the references to the given file
+#'
+#' @return None
+#'
+#' @examples
+#' load_library("R-requirements.txt", "references/R-references.bib")
 
-load_library <- function(packages = NULL, bib_file = "references/R-references.bib") {
-  if (is.null(packages))
-    packages <- c(
-      "base",
-      "bibtex",
-      "tidyverse",
-      "lubridate",
-      "readxl",
-      "knitr",
-      "rmarkdown",
-      "bookdown",
-      "citr",
-      "tinytex",
-      "servr"
-    )
-  new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
-  if(length(new.packages)) install.packages(new.packages)
+load_library <- function(req_file, bib_file = NULL) {
+  packages <- readLines(req_file)
+  new.packages <-
+    packages[!(packages %in% installed.packages()[, "Package"])]
   
-  invisible(lapply(packages, library, character.only = TRUE))
+  if (length(new.packages))
+    install.packages(new.packages)
+  invisible(suppressMessages(suppressWarnings(
+    lapply(packages, library, character.only = TRUE)
+  )))
   
-  knitr::write_bib(packages, file = bib_file)
+  
+  if (!is.null(bib_file))
+    knitr::write_bib(packages, file = bib_file)
+  
   return(NULL)
 }
-
-# invisible(suppressMessages(suppressWarnings(load_library())))
-
