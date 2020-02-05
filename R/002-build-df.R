a07b7d3e9106bce28d836c30a0266eb7f81c3d68
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-02-05 16:07:31 +1100

    Combined into two scripts
    To complete building of dataframes

---
+++ R/002-build-df.R
@@ -0,0 +1,211 @@
+#========================= Build Individual Dataframes =========================
+#                                Alwin Wang 2019
+
+#---------------------------- Data Collection Check ----------------------------
+
+#' Check for data collection errors
+#' 
+#' Compares creatinine and oliguria data to look for discrepencies in the 
+#' UR number, Dates screened and Excl criteria ok
+#' 
+#' @param xlsx_data A list containing all the xlsx data
+#' 
+#' @return Prints out possible errors. Returns corrected xlsx data and the 
+#' exlucluded patients
+#' 
+#' @examples
+#' xlsx_data <- data_collection_errors(xlsx_data)
+
+data_collection_errors <- function(xlsx_data) {
+  errors_logi =
+    (xlsx_data$creatinine$screen_log$`UR number` != 
+       xlsx_data$oliguria$screen_log$`UR number`) |
+    (xlsx_data$creatinine$screen_log$Dates_screened != 
+       xlsx_data$oliguria$screen_log$Dates_screened)  |
+    (xlsx_data$creatinine$screen_log$Excl_criteria_ok != 
+       xlsx_data$oliguria$screen_log$Excl_criteria_ok)
+  
+  creatinine_errors = xlsx_data$creatinine$screen_log[errors_logi, ]
+  oliguria_errors = xlsx_data$oliguria$screen_log[errors_logi, ]
+  
+  xlsx_data$excluded_UR_numbers = creatinine_errors$`UR number`
+  xlsx_data$excluded_Pt_Study_no = discard(
+    c(oliguria_errors$Pt_Study_no, creatinine_errors$Pt_Study_no),
+    is.na)
+  
+  cat("Creatinine xlsx sheet\n")
+  print(creatinine_errors[, c(13, 2:4)])
+  cat("\nOliguria xlsx sheet\n")
+  print(oliguria_errors[, c(13, 2:4)])
+  cat(paste(
+    "\n",
+    # "Discarded UR Numbers:       ", paste(xlsx_data$excluded_UR_numbers , collapse = ", "), "\n",
+    "Discarded Pt Study Numbers: ",
+    paste(xlsx_data$excluded_Pt_Study_no, collapse = ", "),
+    "\n"
+  ))
+  
+  # Correct data  
+  xlsx_data$creatinine$screen_log[errors_logi, "Dates_screened"] =
+    xlsx_data$oliguria$screen_log[errors_logi, "Dates_screened"]
+  
+  xlsx_data$creatinine$screen_log[errors_logi, "Excl_criteria_ok"] = "N"
+  xlsx_data$creatinine$screen_log[errors_logi, "Incl_criteria_ok"] = NA
+  xlsx_data$creatinine$screen_log[errors_logi, "Already_AKI"] = "Y"
+  xlsx_data$creatinine$screen_log[errors_logi, "Epis_cr_change"] = NA
+  xlsx_data$creatinine$screen_log[errors_logi, "Total_no_cr_epis"] = NA
+  
+  xlsx_data$oliguria$screen_log[errors_logi, "Excl_criteria_ok"] = "N"
+  xlsx_data$oliguria$screen_log[errors_logi, "Incl_criteria_ok"] = NA
+  xlsx_data$oliguria$screen_log[errors_logi, "Already_AKI"] = "Y"
+  xlsx_data$oliguria$screen_log[errors_logi, "Epis_olig"] = NA
+  xlsx_data$oliguria$screen_log[errors_logi, "Total_no_olig_epis"] = NA
+  
+  return(xlsx_data)
+}
+
+#---------------------------- Merge Screening Logs -----------------------------
+
+#' Merge the screening log Excel sheets from creatinine and oliguria
+#' 
+#' Takes the xlsx data and merges the creatinine and oliguria screening logs 
+#' together using all columns except incl criteria, pt study number and comment.
+#' Turns binary data into TRUE / FALSE / NA
+#' Note there will also be duplicate UR numbers in here due to readmissions
+#' 
+#' @param creatinine_screen_log Creatinine screening log
+#' @param oliguria_screen_log Oliguria screening log
+#' 
+#' @return A dataframe of screening logs combined from creatinine and oliguria
+#' 
+#' @examples 
+#' screening_data <- merge_xlsx_screening(xlsx_data$creatinine$screen_log,
+#'                                        xlsx_data$oliguria$screen_log)
+
+merge_xlsx_screening <- function(creatinine_screen_log,
+                                 oliguria_screen_log) {
+  # Merge all columns except incl criteria, pt study number and comment
+  merge_columns = setdiff(intersect(
+    colnames(creatinine_screen_log),
+    colnames(oliguria_screen_log)
+  ),
+  c("Incl_criteria_ok", "Pt_Study_no", "Comment"))
+  
+  screening_data = full_join(
+    creatinine_screen_log,
+    oliguria_screen_log,
+    by     = merge_columns,
+    suffix = c("_crch", "_olig")
+  )
+  
+  # Check output
+  if (anyNA(screening_data$`UR number`)) {
+    stop("NA in UR number of merged Excel sheets")
+  }
+  creatinine_n_obs = nrow(creatinine_screen_log)
+  oliguria_n_obs   = nrow(oliguria_screen_log)
+  screening_n_obs  = nrow(screening_data)
+  if (length(unique(c(creatinine_n_obs, oliguria_n_obs, screening_n_obs))) != 1) {
+    stop(paste0(
+      "Inconsistent number of n_obs after UR numbers discarded. ",
+      "Creatinine: ", creatinine_n_obs, ", ",
+      "Oliguria: "  , oliguria_n_obs  , ", ",
+      "Merged: "    , screening_n_obs , ", "
+      ))
+  }
+  
+  # Turn columns into TRUE/FALSE/NA logicals except some
+  logi_colnames <- colnames(screening_data)[!grepl(
+    "UR number|Dates_screened|Pt_Study_no|Total_no_|Comment",
+    colnames(screening_data)
+  )]
+  screening_data <- screening_data %>%
+    mutate_at(logi_colnames, function(x)
+      if_else(x == "N", FALSE, TRUE))
+  
+  # Add excluded Pt Study No
+  # Could be smarter and have LX1, LX2 in chronological order
+  screening_data <- screening_data %>% 
+    mutate(
+      Pt_Study_no_excl = is.na(Pt_Study_no_crch) & is.na(Pt_Study_no_olig)) %>% 
+    arrange(-Pt_Study_no_excl) %>% 
+    mutate(
+      Pt_Study_no_excl = ifelse(Pt_Study_no_excl, paste0("LX", 1:n()), NA)
+    ) %>% 
+    arrange(`UR number`)
+  
+  return(screening_data)
+}
+
+#----------------------------- Patient Study Number ----------------------------
+
+#' Generate Pt Study Number
+#' 
+#' Generates a dataframe of patient study number, UR number and Dates Screened
+#' information
+#' 
+#' @param screening_data The screening log dataframe
+#' 
+#' @return Dataframe of reduced screening_data with no 
+#' 
+#' @examples 
+#' pt_study_no <- generate_pt_study_no(screening_data)
+
+generate_pt_study_no <- function(screening_data) {
+  pt_study_no <- screening_data %>% 
+    select(`UR number`, Pt_Study_no_crch, Pt_Study_no_olig, 
+           Pt_Study_no_excl, Dates_screened)
+  
+  pt_study_no <- distinct(pt_study_no)
+  
+  return(pt_study_no)
+}
+
+#-------------------------- Merge Patient Information --------------------------
+
+#' Find column names
+#' 
+#' Find columns containing a text phrase at the start or end
+#' 
+#' @param text A string containing the search phrase
+#' @param replace A string to replace the text search phrase
+#' @param colnames A vector of column names to search through
+#' 
+#' @return A dataframe of column number, original column name found and the
+#' replaced matching column name
+#' 
+#' @examples 
+#' find_cols("date", "DateTime", c("date_start", "end_date", "mid_date_"))
+
+find_cols <- function(text, replace, colnames) {
+  cols <- data.frame(
+    i = grep(paste0("^", text, "|", text, "$"), colnames, ignore.case = TRUE),
+    j = grep(paste0("^", text, "|", text, "$"), colnames, ignore.case = TRUE, value = TRUE),
+    stringsAsFactors = FALSE
+  ) %>% 
+    mutate(k = gsub(text, replace, j, ignore.case = TRUE))
+  colnames(cols) <- c(paste0(text, "_i"), paste0(text), "match")
+  
+  return(cols)
+}
+
+#' Merge patient information
+#' 
+#' Merge patient demographics from various sources
+#' 
+#' @param creatinine_demographic Creatinine demographics
+#' @param oliguria_demographic Oliguria demographics
+#' @param pt_study_no Patient study number IDs
+#' 
+#' @return A dataframe of patient information
+#' 
+#' @example 
+#' patient_data = merge_xlsx_pt_info(xlsx_data$creatinine$demographic,
+#'                                   xlsx_data$oliguria$demographic)
+
+
+merge_xlsx_pt_info <- function(creatinine_demographic,
+                               oliguria_demographic) {
+  
+}
+
--- main.R
+++ main.R
@@ -1,18 +1,32 @@
 #============================= Main Analysis Runner ============================
 #                                Alwin Wang 2019
 
+#------------------------------------ About ------------------------------------
+# 1. Setup: Source data, check for errors, etc.
+# 2. Build DFs: ID | Pt Info | Cre | Oli | Outcome
+# 3. Build List: ID:{...}
+
 #------------------------------- Source Scripts --------------------------------
-source("R/000-data-source.R")
-source("R/001-library-manager.R")
-source("R/002-merge-data.R")
+source("R/001-set-up.R")
+source("R/002-build-df.R")
 
 #-------------------------------- Run Analysis ---------------------------------
+# 1 Setup
 invisible(suppressMessages(
   load_library("R-requirements.txt", "R-references.bib")
 ))
 
 xlsx_data <- suppressMessages(load_excel_files())
+names(xlsx_data)
 xlsx_data <- data_collection_errors(xlsx_data)
 
-screening_data <- merge_xlsx_screening(xlsx_data)
+# 2 Build DFs
+screening_data <- merge_xlsx_screening(
+  xlsx_data$creatinine$screen_log, xlsx_data$oliguria$screen_log)
+glimpse(screening_data, width = 80)
+
+pt_study_no <- generate_pt_study_no(screening_data)
+print(pt_study_no)
+
+
 merged_data <- merge_xlsx_creatinine_oliguria(screening_data, xlsx_data)
