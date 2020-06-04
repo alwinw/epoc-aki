8b55584112f7b8654c71d0b6df4a65d0cc445f5c
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-06-04 21:34:58 +1000

    Better DateTime convertion method started

--- R/scratch.R
+++ R/scratch.R
@@ -1,20 +1,33 @@
 xlsx_data$creatinine$data_set
 
-creatinine_data <- xlsx_data$creatinine$data_set %>% 
-  rename(
-    Pt_Study_no_crch = Pt_Study_no,
-    
-  ) %>% 
-  mutate(Epis_cr_change = "Y")
 
-oliguria_data <- xlsx_data$oliguria$data_set
+
 
 setdiff(colnames(creatinine_data), colnames(oliguria_data))
 
 colnames(creatinine_data)
 
 dttm_col = inner_join(
-  find_cols("date", "DateTime", colnames(creatinine_data)),
-  find_cols("time", "DateTime", colnames(creatinine_data)), 
+  find_cols("date", "DateTime", colnames(data_set)),
+  find_cols("time", "DateTime", colnames(data_set)), 
   by = "match") %>% 
   select(date, time, match)
+
+colnames(data_set)
+
+test <- data_set[, c("Pt_Study_no", "Epis_no", dttm_col$date, dttm_col$time)] %>% 
+  pivot_longer(
+    all_of(c(dttm_col$date, dttm_col$time)),
+    names_to = "DateTimeName",
+    values_to = "DateTime"
+  ) %>% 
+  mutate(
+    DateTimeType = if_else(grepl("^time|time$", DateTimeName, ignore.case = TRUE), "Time", ""),
+    DateTimeType = if_else(grepl("^date|date$", DateTimeName, ignore.case = TRUE), "Date", DateTimeType),
+    DateTimeName = gsub("^time|time$|^date|date$", "DateTime", DateTimeName, ignore.case = TRUE)
+  ) %>% 
+  pivot_wider(
+    names_from = "DateTimeType",
+    values_from = "DateTime"
+  )  
+
