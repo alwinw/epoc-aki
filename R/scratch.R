77bb6ab0689427401982331b735037c059f62d9e
Alwin <16846521+AlwinW@users.noreply.github.com>
2020-06-05 07:39:32 +1000

    DateTime checks
    Need to correct two time entries

--- R/scratch.R
+++ R/scratch.R
@@ -1,21 +1,6 @@
-xlsx_data$creatinine$data_set
-
-
-
-
-setdiff(colnames(creatinine_data), colnames(oliguria_data))
-
-colnames(creatinine_data)
-
-dttm_col = inner_join(
-  find_cols("date", "DateTime", colnames(data_set)),
-  find_cols("time", "DateTime", colnames(data_set)), 
-  by = "match") %>% 
-  select(date, time, match)
-
-colnames(data_set)
-
-test <- data_set[, c("Pt_Study_no", "Epis_no", dttm_col$date, dttm_col$time)] %>% 
+test <- rbind(creatinine_data,  oliguria_data) %>% 
+  fill(Pt_Study_no, .direction = "down") %>% 
+  select(all_of(c("Pt_Study_no", "Epis_no", dttm_col$date, dttm_col$time))) %>% 
   pivot_longer(
     all_of(c(dttm_col$date, dttm_col$time)),
     names_to = "DateTimeName",
@@ -29,5 +14,23 @@ test <- data_set[, c("Pt_Study_no", "Epis_no", dttm_col$date, dttm_col$time)] %>
   pivot_wider(
     names_from = "DateTimeType",
     values_from = "DateTime"
-  )  
+  ) %>% 
+  mutate(
+    datetime = ifelse(
+      (is.na(Date) | is.na(Time)),
+      NA,
+      paste(format(Date, format = "%Y-%m-%d"), format(Time, format = "%H:%M:%S"))),
+    Date = NULL,
+    Time = NULL
+  ) %>% 
+  mutate(datetime = as_datetime(datetime, tz = "Australia/Melbourne"))
 
+
+test <- data_set %>% 
+  select(starts_with("date") | starts_with("time") | ends_with("time")) %>% 
+  mutate(
+    T0_corresp_DateTime = ceiling_date(DateTime_epis, "hour"),
+    T0_corresp_check = format(T0_corresp_time, format = "%H:%M:%S") == 
+      format(T0_corresp_DateTime, format = "%H:%M:%S")
+  ) %>% 
+  filter(!T0_corresp_check)
