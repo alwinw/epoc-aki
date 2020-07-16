da786917381e52a7e30b18964fb3cb8039d102ff
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-07-16 22:29:20 +1000

    Stepwise analysis

--- R/13_cr_ch_only.R
+++ R/13_cr_ch_only.R
@@ -23,8 +23,8 @@ cr_ch_model <- function(lower_hr_del_t_ch, upper_hr_del_t_ch, hr_before_aki, plo
   logit_ts <- logit_df %>%
     filter(
       duration(hour = lower_hr_del_t_ch) < del_t_ch,
-      del_t_ch < duration(hour = upper_hr_del_t_ch),
-      is.na(del_t_aki) | del_t_aki > duration(minute = hr_before_aki)
+      del_t_ch <= duration(hour = upper_hr_del_t_ch),
+      is.na(del_t_aki) | del_t_aki >= duration(minute = hr_before_aki)
     )
 
   logit_model <- glm(
@@ -58,6 +58,11 @@ cr_ch_model <- function(lower_hr_del_t_ch, upper_hr_del_t_ch, hr_before_aki, plo
   ))
 }
 
+# ---- generator_function ----
+gen_cr_ch_model <- function(lower, step, upper, hr_before_aki) {
+
+}
+
 cr_ch_model(lower_hr_del_t_ch = 3.5, upper_hr_del_t_ch = 4.5, hr_before_aki = 1/60)
 cr_ch_model(lower_hr_del_t_ch = 4.5, upper_hr_del_t_ch = 5.5, hr_before_aki = 1/60)
 cr_ch_model(lower_hr_del_t_ch = 5.5, upper_hr_del_t_ch = 6.5, hr_before_aki = 1/60)
@@ -69,3 +74,47 @@ cr_ch_model(lower_hr_del_t_ch = 5.5, upper_hr_del_t_ch = 8.5, hr_before_aki = 1/
 
 
 cr_ch_model(lower_hr_del_t_ch = 6.5, upper_hr_del_t_ch = 12.5, hr_before_aki = 1/60)
+
+
+cr_ch_steps = seq(0, 20, by = 1)
+cr_ch_steps_df = data.frame(
+  lower_hr_del_t_ch = head(cr_ch_steps, -1),
+  upper_hr_del_t_ch = tail(cr_ch_steps, -1),
+  hr_before_aki     = 12
+) %>%
+  mutate(del_t_ch_range = paste0("[", lower_hr_del_t_ch, ", ", upper_hr_del_t_ch, "]"))
+
+cr_ch_steps_df %>%
+  rowwise() %>%
+  do(data.frame(., cr_ch_model(.$lower_hr_del_t_ch, .$upper_hr_del_t_ch, .$hr_before_aki))) %>%
+  ungroup() %>%
+  arrange(desc(AUC))
+
+cr_ch_steps = seq(0.5, 20, by = 1)
+cr_ch_steps_df = data.frame(
+  lower_hr_del_t_ch = head(cr_ch_steps, -1),
+  upper_hr_del_t_ch = tail(cr_ch_steps, -1),
+  hr_before_aki     = 12
+) %>%
+  mutate(del_t_ch_range = paste0("[", lower_hr_del_t_ch, ", ", upper_hr_del_t_ch, "]"))
+
+cr_ch_steps_df %>%
+  rowwise() %>%
+  do(data.frame(., cr_ch_model(.$lower_hr_del_t_ch, .$upper_hr_del_t_ch, .$hr_before_aki))) %>%
+  ungroup() %>%
+  arrange(desc(AUC))
+
+
+cr_ch_steps = seq(0, 20, by = 0.5)
+cr_ch_steps_df = data.frame(
+  lower_hr_del_t_ch = head(cr_ch_steps, -1),
+  upper_hr_del_t_ch = tail(cr_ch_steps, -1),
+  hr_before_aki     = 12
+) %>%
+  mutate(del_t_ch_range = paste0("[", lower_hr_del_t_ch, ", ", upper_hr_del_t_ch, "]"))
+
+cr_ch_steps_df %>%
+  rowwise() %>%
+  do(data.frame(., cr_ch_model(.$lower_hr_del_t_ch, .$upper_hr_del_t_ch, .$hr_before_aki))) %>%
+  ungroup() %>%
+  arrange(desc(AUC))
