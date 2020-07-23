262cbab89cf1e8257b8a496c409df46a392e3fa0
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-07-23 11:43:25 +1000

    Added an upper bound to the t_aki model selection

--- R/20_cr_ch_only.R
+++ R/20_cr_ch_only.R
@@ -16,17 +16,24 @@ logit_df <- admission_ts %>%
     APACHE_III = if_else(is.na(APACHE_III), median(APACHE_III,na.rm = TRUE), APACHE_III)
   ) %>%   # FIXME Replace with REAL data
   ungroup() %>%
-  filter(abs(del_cr) < 50)
+  filter(abs(del_cr) < 50) %>%  # Consider if this is reasonable or not
+  mutate(
+    del_t_ch_hr  = as.numeric(del_t_ch, "hours"),
+    del_t_aki_hr = as.numeric(del_t_aki, "hours"))
 
 # ---- cr_ch_function ----
-cr_ch_model <- function(lower_hr_del_t_ch, upper_hr_del_t_ch, hr_before_aki, plot = FALSE) {
+cr_ch_model <- function(vec_del_t_ch_hr, vec_del_t_aki_hr, plot = FALSE) {
+  vec_del_t_ch_hr  = sort(vec_del_t_ch_hr)
+  vec_del_t_aki_hr = sort(vec_del_t_aki_hr)
   logit_ts <- logit_df %>%
     filter(
-      duration(hour = lower_hr_del_t_ch) < del_t_ch,
-      del_t_ch <= duration(hour = upper_hr_del_t_ch),
-      is.na(del_t_aki) | del_t_aki >= duration(minute = hr_before_aki)
+      del_t_ch_hr >= vec_del_t_ch_hr[1],
+      del_t_ch_hr <= vec_del_t_ch_hr[2],
+      is.na(del_t_aki) |
+        del_t_aki_hr >= vec_del_t_aki_hr[1] &
+        del_t_aki_hr <= vec_del_t_aki_hr[2]
     )
-  if(nrow(logit_ts) == 0 | lower_hr_del_t_ch < 0){
+  if(nrow(logit_ts) == 0 | vec_del_t_ch_hr[1] < 0 | vec_del_t_aki_hr[1] < 0){
     return(data.frame(
       AUC              = 0,
       sensitivity      = 0,
@@ -83,9 +90,11 @@ cr_ch_model <- function(lower_hr_del_t_ch, upper_hr_del_t_ch, hr_before_aki, plo
 
 # ---- example_1 ----
 crch_window = 1
-crch_centre = 4
+crch_centre = 6.5
 hr_before_aki = 8
-result_df <- cr_ch_model(crch_centre - crch_window/2, crch_centre + crch_window/2, hr_before_aki) %>%
+hr_after_aki = 16
+
+result_df <- cr_ch_model(c(crch_centre - crch_window/2, crch_centre + crch_window/2), c(hr_before_aki, hr_after_aki)) %>%
   pivot_longer(
     ends_with("pos")|ends_with("neg"),
     names_to = c("admission", "heatmap"), values_to = "count",
@@ -96,15 +105,19 @@ result_df <- cr_ch_model(crch_centre - crch_window/2, crch_centre + crch_window/
     values_from = count
   ) %>%
   mutate(
-    heatmap = if_else(heatmap == "neg", "No AKI", paste0("T_AKI in ", hr_before_aki, "+hrs after cr_ch"))
+    heatmap = if_else(
+      heatmap == "neg",
+      "No AKI",
+      paste0("T_AKI in  ", hr_before_aki, "-", hr_after_aki, "hrs after cr_ch"))
   )
 
-heatmap_all <- admission_ts %>%
-  filter(is.na(del_t_aki) | del_t_aki > hr_before_aki) %>%
+heatmap_all <- logit_df %>%
+  # filter(is.na(del_t_aki_hr) | del_t_aki_hr > hr_before_aki) %>%
   mutate(
-    heatmap = case_when(
-      is.na(del_t_aki)          ~ "No AKI",
-      del_t_aki > hr_before_aki ~ paste0("T_AKI in ", hr_before_aki, "+hrs after cr_ch"),
+    heatmap = case_when(  # TODO change to factor and make easier to use
+      is.na(del_t_aki_hr)          ~ "No AKI",
+      del_t_aki_hr > hr_after_aki  ~ paste0("T_AKI in ", hr_after_aki, "+hrs after cr_ch"),
+      del_t_aki_hr > hr_before_aki ~ paste0("T_AKI in  ", hr_before_aki, "-", hr_after_aki, "hrs after cr_ch"),
       TRUE                      ~ NA_character_
     ),
   )
@@ -114,32 +127,31 @@ heatmap_count <- heatmap_all %>%
   summarise(n_cr = n(), n_admission = n_distinct(AdmissionID), .groups = "keep")
 
 heatmap_ts <- heatmap_all %>%
-  mutate(del_t_ch = as.numeric(del_t_ch, "hours")) %>%
-  filter(del_t_ch < 13, abs(del_cr) < 50) %>%
-  select(del_t_ch, del_cr, heatmap)
+  filter(del_t_ch_hr < 13, abs(del_cr) < 50) %>%
+  select(del_t_ch_hr, del_cr, heatmap)
 
-ggplot(heatmap_ts, aes(x = del_t_ch, y = del_cr)) +
+ggplot(heatmap_ts, aes(x = del_t_ch_hr, y = del_cr)) +
   geom_density_2d_filled(contour_var = "density") +
   geom_hline(yintercept = 0, colour = "white", linetype = "dotted") +
   annotate("tile", x = crch_centre, y = 2.5, width = crch_window, height = 55,
     fill = "white", colour = NA, alpha = 0.1, size = 0.2
   ) +
   annotate(
-    "segment", 
-    x    = c(crch_centre - crch_window/2, crch_centre + crch_window/2), 
-    xend = c(crch_centre - crch_window/2, crch_centre + crch_window/2), 
-    y = c(-22, -22), 
+    "segment",
+    x    = c(crch_centre - crch_window/2, crch_centre + crch_window/2),
+    xend = c(crch_centre - crch_window/2, crch_centre + crch_window/2),
+    y = c(-22, -22),
     yend = c(22, 22),
     colour = "white"
-  ) + 
+  ) +
   geom_vline(xintercept = crch_centre, colour = "white", linetype = "dotted") +
   geom_text(
-    aes(x = crch_centre - crch_window/2 - 0.5, y = 22,
+    aes(x = crch_centre, y = 22,
         label = paste0("Captured cr_ch: ", crch_centre - crch_window/2, " < \u0394 t < ", crch_centre + crch_window/2,
                        "\nn(Admissions): ", admissions,
                        "\nAUC: ", round(AUC, 4))),
     data = result_df,
-    colour = "white", hjust = -0, vjust = -0.1,
+    colour = "white", hjust = 0.5, vjust = -0.1,
   ) +
   geom_text(
     aes(x = 0.2, y = -24,
@@ -147,7 +159,7 @@ ggplot(heatmap_ts, aes(x = del_t_ch, y = del_cr)) +
     data = heatmap_count,
     colour = "white", hjust = 0, vjust = 0
   ) +
-  facet_wrap(~heatmap, nrow = 1) +
+  facet_wrap(~heatmap, nrow = 2) +
   scale_x_continuous(breaks = seq(0, 12, by = 2)) +
   coord_cartesian(xlim = c(0, 12), ylim = c(-25, 30), expand = FALSE) +
   ggtitle("Creatinine Changes") +
