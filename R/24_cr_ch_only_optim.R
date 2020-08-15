3319f7e4265f6cd9dd582b33d1d210742e875cbd
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-08-15 21:59:54 +1000

    Optim for cr_ch only and cr_ch multi

--- R/24_cr_ch_only_optim.R
+++ R/24_cr_ch_only_optim.R
@@ -1,4 +1,4 @@
-# ---- cr_ch_multi_plot_df ----
+# ---- cr_ch_only_plot_df ----
 density = 4
 centre_spacing = 0.15/sqrt(density)
 
@@ -21,11 +21,8 @@ cr_ch_dump <- pblapply(
   cr_ch_list, function(df) {
     data.frame(df, analysis_wrapper(
       outcome_var = "AKI_ICU",
-      baseline_predictors = c(
-       "Age + APACHE_II + APACHE_III + Baseline_Cr",
-       "PCs_cardio + Vasopressor + Diabetes + AF + IHD + HF + HT + PVD + Chronic_liver_disease"
-      ),
-      cr_predictors = "cr",
+      baseline_predictors = "",
+      cr_predictors = "",
       del_t_ch_hr_range = c(df$lower, df$upper),
       del_t_aki_hr_range = c(8, 16),
       add_gradient_predictor = 1,
@@ -37,11 +34,10 @@ cr_ch_dump <- pblapply(
   cl = cl
 )
 stopCluster(cl)
-rm(cl)
+rm(density, centre_spacing, cr_ch_grid, cr_ch_list, cl)
 
 heuristic_calc <- function(AUC, per_admin_in){
-  return((1/2 + 1/2*tanh(11*(AUC - 0.875 + 0.1)) +
-        1/2 + 1/2*tanh(11*(per_admin_in - 0.525 + 0.1)))/2)
+  return(AUC)
 }
 
 cr_ch_plot <- bind_rows(cr_ch_dump) %>%
@@ -50,17 +46,17 @@ cr_ch_plot <- bind_rows(cr_ch_dump) %>%
   mutate(heuristic = heuristic_calc(AUC, per_admin_in)) %>%
   arrange(centre)
 
-cr_ch_plot %>%
-  arrange(desc(heuristic)) %>%
-  filter(centre > 4) %>%
-  head(., 5)
+# cr_ch_plot %>%
+#   arrange(desc(heuristic)) %>%
+#   filter(centre > 4) %>%
+#   head(., 5)
 
-# ---- cr_ch_multi_AUC_n_plot ----
+
+# ---- cr_ch_only_AUC_n_plot ----
 ggplot(cr_ch_plot, aes(centre, width)) +
   geom_contour_fill(aes(z = AUC), bins = 32, alpha = 1, na.fill = TRUE) +
   geom_contour(aes(z = n_admissions), colour = "white", binwidth = 20) +
   geom_text_contour(aes(z = n_admissions), colour = "white") +
-  # geom_point(shape = 1, fill = NA, colour = "white", alpha = 0.1) +
   scale_x_continuous(
     breaks = seq(0, 20, by = 1),
     limits = c(min(cr_ch_plot$centre), max(cr_ch_plot$centre)),
@@ -71,31 +67,14 @@ ggplot(cr_ch_plot, aes(centre, width)) +
   ggtitle("AUC (Colour) and Number of Included Admissions (Contour Lines)")
 
 
-# ---- cr_ch_multi_heuristic_plot ----
-ggplot(cr_ch_plot, aes(centre, width)) +
-  geom_contour_fill(aes(z = heuristic), bins = 32, alpha = 1, na.fill = TRUE) +
-  geom_point(shape = 1, fill = NA, colour = "white", alpha = 0.1) +
-  scale_x_continuous(
-    breaks = seq(0, 20, by = 1),
-    limits = c(min(cr_ch_plot$centre), max(cr_ch_plot$centre)),
-    expand = c(0, 0)
-  ) +
-  scale_y_continuous(expand = c(0, 0)) +
-  scale_fill_viridis_c(name = "Heuristic", option = "C") +
-  ggtitle("Heuristic Plot")
-
-
-# ---- optimisation ----
+# ---- cr_ch_only_optimisation ----
 optim_one <- optim(
   c(6, 2, 8, 10),
   function(x) {
     output = analysis_wrapper(
       outcome_var = "AKI_ICU",
-      baseline_predictors = c(
-        "Age + APACHE_II + APACHE_III + Baseline_Cr",
-        "PCs_cardio + Vasopressor + Diabetes + AF + IHD + HF + HT + PVD + Chronic_liver_disease"
-      ),
-      cr_predictors = "cr",
+      baseline_predictors = "",
+      cr_predictors = "",
       del_t_ch_hr_range = c(x[1] - x[2]/2, x[1] + x[2]),
       del_t_aki_hr_range = c(x[3], x[3] + x[4]),
       add_gradient_predictor = 1,
@@ -111,22 +90,15 @@ optim_one <- optim(
 
 set.seed(8)
 optim_in = rbind(
-  c(6,   1.5, 8, 10),
-  c(6.8, 1.8, 8, 10),
-  c(5,   1.5, 8, 10),
-  c(6.5, 2.5, 8, 8 ),
-  c(6.90, 1.96, 8.77, 34.75),
+  c(6.0, 1.0, 8.0, 10 ), # Baseline
+  c(6.0, 2.1, 8.1, 4.3), # AUC 0.6451
+  c(6.6, 2.5, 8.0, 4.0), # AUC 0.6637
+  c(6.7, 1.2, 8.1, 9.6), # AUC 0.6396
   cbind(
     runif(20, 6, 7),
-    runif(20, 1, 3),
-    runif(20, 8, 9),
-    runif(20, 10, 11)
-  ),
-  cbind(
-    runif(20, 6.5, 7.5),
     runif(20, 1.5, 2.5),
-    runif(20, 8.5, 10.5),
-    runif(20, 30, 45)
+    runif(20, 7.5, 8.5),
+    runif(20, 4, 5)
   ),
   cbind(
     runif(50, 3, 10),
@@ -144,11 +116,8 @@ optim_out <- multistart(
     aki_max = x[3] + x[4]
     output = analysis_wrapper(
       outcome_var = "AKI_ICU",
-      baseline_predictors = c(
-        "Age + APACHE_II + APACHE_III + Baseline_Cr",
-        "PCs_cardio + Vasopressor + Diabetes + AF + IHD + HF + HT + PVD + Chronic_liver_disease"
-      ),
-      cr_predictors = "cr",
+      baseline_predictors = "",
+      cr_predictors = "",
       del_t_ch_hr_range = c(ch_min, ch_max),
       del_t_aki_hr_range = c(aki_min, aki_max),
       add_gradient_predictor = 1,
@@ -157,11 +126,11 @@ optim_out <- multistart(
     )
     return(-heuristic_calc(output$AUC, output$per_admin_in))
   },
-  lower = c(3, 0.1, 8, 4),
+  lower = c(3, 0.1, 8, 3),
   upper = c(10, 3, 12, 48),
   method = "L-BFGS-B"
 )
-optim_summary <- optim_out %>%
+optim_cr_ch_only <- optim_out %>%
   arrange(value) %>%
   mutate(
     ch_hr_lower = p1 - p2/2,
@@ -174,9 +143,8 @@ optim_summary <- optim_out %>%
     .,
     analysis_wrapper(
       outcome_var = "AKI_ICU",
-      baseline_predictors = c("Age + APACHE_II + APACHE_III + Baseline_Cr",
-                              "PCs_cardio + Vasopressor + Diabetes + AF + IHD + HF + HT + PVD + Chronic_liver_disease"),
-      cr_predictors = "cr",
+      baseline_predictors = "",
+      cr_predictors = "",
       del_t_ch_hr_range = c(.$ch_hr_lower, .$ch_hr_upper),
       del_t_aki_hr_range = c(.$aki_hr_lower, .$aki_hr_upper),
       add_gradient_predictor = 1,
@@ -189,4 +157,7 @@ optim_summary <- optim_out %>%
   mutate(
     heuristic = heuristic_calc(AUC, per_admin_in)
   ) %>%
-  select(-p1:-p4, -fevals:-convergence)
+  select(-p1:-p4, -fevals:-convergence, -value)
+
+rm(cr_ch_dump, optim_one, optim_in, optim_out)
+rm(heuristic_calc)
