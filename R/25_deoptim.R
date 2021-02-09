27abd4cc856d5a00991a42984d2b572d31e80747
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-02-09 17:29:44 +1100

    Initial deoptim_search run

--- R/25_deoptim.R
+++ R/25_deoptim.R
@@ -190,31 +190,12 @@ multi_model <- deoptim_search(
   stepwise = TRUE, ## CHANGE THIS BACK
   k = "mBIC",
   penalty_fn = heuristic_penalty,
-  itermax = 2,
-  NP = 8,
+  itermax = 200,
+  NP = 320,
   parallel = TRUE,
   secondary_outcomes = c(
     "AKI_ICU",
     "Cr_defined_AKI_2or3", "Cr_defined_AKI",
     "Olig_defined_AKI_2or3", "Olig_defined_AKI"
   )
-)
-
-outcome_var <- "AKI_2or3"
-baseline_predictors <- c(
-  "Age + Male + APACHE_II + APACHE_III + Baseline_Cr",
-  "PCs_cardio + Vasopressor + Diabetes + AF + IHD + HF + PVD + Chronic_liver_disease" # HT excluded
-)
-cr_predictors <- "cr"
-add_gradient_predictor <- 1
-stepwise <- TRUE ## CHANGE THIS BACK
-k <- "mBIC"
-penalty_fn <- heuristic_penalty
-itermax <- 2
-NP <- 8
-parallel <- TRUE
-secondary_outcomes <- c(
-  "AKI_ICU",
-  "Cr_defined_AKI_2or3", "Cr_defined_AKI",
-  "Olig_defined_AKI_2or3", "Olig_defined_AKI"
 )
