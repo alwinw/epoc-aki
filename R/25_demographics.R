e99dfe0953b6748e17179b11bb57ef325e45fb5a
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-02-13 23:39:29 +1100

    Example demographics code

--- R/25_demographics.R
+++ R/25_demographics.R
@@ -43,8 +43,54 @@ demographics_df %>%
 wilcox.test(APACHE_II ~ Epis_cr, demographics_df)
 
 
-demographics_df %>%
+cont_iqr <- demographics_df %>%
   select(
     AKI_2or3,
     Age, Wt, APACHE_II, APACHE_III
-  )
+  ) %>% 
+  pivot_longer(-AKI_2or3, names_to = "variable") %>% 
+  group_by(AKI_2or3, variable) %>% 
+  summarise(
+    median = median(value, na.rm = TRUE),
+    q1 = quantile(value, 0.25, na.rm = TRUE),
+    q3 = quantile(value, 0.75, na.rm = TRUE),
+    .groups = "drop"
+  ) %>% 
+  mutate(text = sprintf("%.1f (%.1f - %.1f)", median, q1, q3)) %>% 
+  select(AKI_2or3, variable, text) %>% 
+  pivot_wider(names_from = AKI_2or3, values_from = text)
+
+cont_pval <- demographics_df %>%
+  select(
+    AKI_2or3,
+    Age, Wt, APACHE_II, APACHE_III
+  ) %>% 
+  pivot_longer(-AKI_2or3, names_to = "variable") %>% 
+  group_by(variable) %>% 
+  summarise(pval = wilcox.test(value ~ AKI_2or3)$p.value, .groups = "drop") %>%
+  select(variable, pval)
+
+bin_n <- demographics_df %>% 
+  select(
+    AKI_2or3,
+    Male, Mecvenadm
+  ) %>% 
+  pivot_longer(-AKI_2or3, names_to = "variable") %>% 
+  group_by(AKI_2or3, variable, value) %>% 
+  summarise(n = n(), .groups = "drop") %>% 
+  group_by(AKI_2or3, variable) %>% 
+  mutate(text = sprintf("%d (%.1f%%)", n, n/sum(n)*100)) %>% 
+  filter(value == 1) %>%  # might have to check for NAs?
+  select(-value, -n) %>% 
+  pivot_wider(names_from = AKI_2or3, values_from = text)
+
+bin_pval <- demographics_df %>% 
+  select(
+    AKI_2or3,
+    Male, Mecvenadm
+  ) %>% 
+  pivot_longer(-AKI_2or3, names_to = "variable") %>% 
+  group_by(variable) %>% 
+  summarise(pval = chisq.test(AKI_2or3, value)$p.value, .groups = "drop")
+
+
