75e5ba10c3171e0450948f471c83c98e63d4e008
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-02-14 10:10:37 +1100

    Split up deoptim

---
+++ R/25_deoptim_run.R
@@ -0,0 +1,21 @@
+set.seed(8)
+multi_model <- deoptim_search(
+  outcome_var = "AKI_2or3",
+  baseline_predictors = c(
+    "Age + Male + APACHE_II + APACHE_III + Baseline_Cr",
+    "PCs_cardio + Vasopressor + Diabetes + AF + IHD + HF + PVD + Chronic_liver_disease" # HT excluded
+  ),
+  cr_predictors = "cr",
+  add_gradient_predictor = 1,
+  stepwise = TRUE,
+  k = "mBIC",
+  penalty_fn = heuristic_penalty,
+  itermax = 200,
+  NP = 320,
+  parallel = TRUE,
+  secondary_outcomes = c(
+    "AKI_ICU",
+    "Cr_defined_AKI_2or3", "Cr_defined_AKI",
+    "Olig_defined_AKI_2or3", "Olig_defined_AKI"
+  )
+)
