6a465eac25fc01ac0db01fb646664143add8099e
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-02-14 11:34:35 +1100

    Majority of demographics complete

--- R/26_demographics.R
+++ R/26_demographics.R
@@ -15,31 +15,30 @@ outcome_var <- "AKI_2or3"
 number <- demographics_df %>%
   select({{ outcome_var }}) %>%
   group_by(.data[[outcome_var]]) %>%
-  summarise(n = n(), .groups = "drop") %>%
-  pivot_wider(names_from = {{ outcome_var }}, values_from = n) %>%
-  mutate(variable = "number", all = nrow(demographics_df), pval = NA)
+  summarise(text = sprintf("%d (%.1f%%)", n(), n() / nrow(.) * 100), .groups = "drop") %>%
+  pivot_wider(names_from = {{ outcome_var }}, values_from = text) %>%
+  mutate(
+    variable = "Number",
+    all = sprintf("%d (%.1f%%)", nrow(demographics_df), 100),
+    pval = NA
+  )
 
-cont_vars <- c(
-  "Age", "Wt", "APACHE_II", "APACHE_II"
-)
-
-cont_all <- demographics_df %>%
-  select(all_of(cont_vars)) %>%
-  pivot_longer(everything(), names_to = "variable") %>%
+cont_df <- demographics_df %>%
+  select(
+    {{ outcome_var }},
+    Age, Wt, APACHE_II, APACHE_III, Baseline_Cr
+  ) %>%
+  pivot_longer(-{{ outcome_var }}, names_to = "variable")
+cont_all <- cont_df %>%
+  select(-{{ outcome_var }}) %>%
   group_by(variable) %>%
   summarise(
     all = sprintf(
-      "%.1f (%.1f - %.1f)",
-      median(value, na.rm = TRUE),
-      quantile(value, 0.25, na.rm = TRUE),
-      quantile(value, 0.75, na.rm = TRUE)
-    ),
-    .groups = "drop"
+      "%.1f (%.1f - %.1f)", median(value, na.rm = TRUE),
+      quantile(value, 0.25, na.rm = TRUE), quantile(value, 0.75, na.rm = TRUE)
+    ), .groups = "drop"
   )
-
-cont_iqr <- demographics_df %>%
-  select({{ outcome_var }}, all_of(cont_vars)) %>%
-  pivot_longer(-{{ outcome_var }}, names_to = "variable") %>%
+cont_iqr <- cont_df %>%
   group_by(.data[[outcome_var]], variable) %>%
   summarise(
     text = sprintf(
@@ -51,23 +50,19 @@ cont_iqr <- demographics_df %>%
     .groups = "drop"
   ) %>%
   pivot_wider(names_from = AKI_2or3, values_from = text)
-
-cont_pval <- demographics_df %>%
-  select({{ outcome_var }}, all_of(cont_vars)) %>%
-  pivot_longer(-{{ outcome_var }}, names_to = "variable") %>%
+cont_pval <- cont_df %>%
   group_by(variable) %>%
   summarise(pval = wilcox.test(value ~ AKI_2or3)$p.value, .groups = "drop") %>% # FIXME hard coding
   select(variable, pval)
 
-bin_vars <- c(
-  "AKI_2or3",
-  "Male", "Mecvenadm",
-  "Surgadmission"
-)
-
-bin_n <- demographics_df %>%
-  select(all_of(bin_vars)) %>%
-  pivot_longer(-{{ outcome_var }}, names_to = "variable") %>%
+bin_df <- demographics_df %>%
+  select(
+    {{ outcome_var }},
+    Male, Mecvenadm, PCs_cardio, Surgadmission:PCs_metabolic,
+    Vasopressor:Chronic_liver_disease, RRT
+  ) %>%
+  pivot_longer(-{{ outcome_var }}, names_to = "variable")
+bin_n <- bin_df %>%
   group_by(.data[[outcome_var]], variable, value) %>%
   summarise(n = n(), .groups = "drop") %>%
   group_by(.data[[outcome_var]], variable) %>%
@@ -77,22 +72,29 @@ bin_n <- demographics_df %>%
   mutate(all = sprintf("%d (%.1f%%)", sum(n), sum(n) / nrow(demographics_df) * 100)) %>%
   select(-value, -n) %>%
   pivot_wider(names_from = {{ outcome_var }}, values_from = text)
-
-bin_pval <- demographics_df %>%
-  select(
-    AKI_2or3,
-    Male, Mecvenadm,
-    Surgadmission
-  ) %>%
-  pivot_longer(-AKI_2or3, names_to = "variable") %>%
+bin_pval <- bin_df %>%
   group_by(variable) %>%
-  summarise(pval = chisq.test(AKI_2or3, value)$p.value, .groups = "drop")
+  summarise(
+    pval = tryCatch(chisq.test(AKI_2or3, value)$p.value, error = function(e) NA),
+    .groups = "drop"
+  )
 
 demographics_table <- rbind(
   number,
-  left_join(left_join(cont_iqr, cont_all), cont_pval),
-  left_join(bin_n, bin_pval)
+  full_join(left_join(cont_iqr, cont_all), cont_pval),
+  full_join(bin_n, bin_pval) %>% arrange(variable)
 ) %>%
-  select(variable, all, {{ outcome_var }} := `1`, "No_{{outcome_var}}" := `0`, pval)
+  mutate(
+    all = if_else(is.na(all), "0 (0.0%)", all),
+    `0` = if_else(is.na(`0`), "0 (0.0%)", `0`),
+    `1` = if_else(is.na(`1`), "0 (0.0%)", `1`)
+  ) %>%
+  select(variable, all, {{ outcome_var }} := `1`, "No_{{outcome_var}}" := `0`, pval) %>%
+  mutate(pval = case_when(
+    pval < 0.001 ~ "<0.001",
+    pval < 0.01 ~ "<0.01 ",
+    abs(pval - 0.05) < 0.01 ~ sprintf("%.3f", pval),
+    TRUE ~ sprintf("%.2f ", pval)
+  ))
 
 kable(demographics_table)
