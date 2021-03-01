4955ef0a5443c4d75ccbebe7f25a41de72f66bc6
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-03-01 21:54:18 +1100

    Small adjustments to heatmap plot

--- R/10_summary.R
+++ R/10_summary.R
@@ -51,14 +51,14 @@ plot_cr_ch_heatmap <- function(analysis_df, save_plots) {
     mutate(
       heatmap = case_when(
         is.na(del_t_aki_hr) ~ " No AKI",
-        del_t_aki_hr < 4 ~ "t_AKI in  0-4hrs",
-        del_t_aki_hr < 8 ~ "t_AKI in  4-8hrs",
-        del_t_aki_hr < 12 ~ "t_AKI in  8-12hrs",
-        del_t_aki_hr < 16 ~ "t_AKI in 12-16hrs",
-        del_t_aki_hr < 20 ~ "t_AKI in 16-20hrs",
-        del_t_aki_hr < 24 ~ "t_AKI in 20-24hrs",
-        del_t_aki_hr < 30 ~ "t_AKI in 24-30hrs",
-        TRUE ~ "t_AKI in 30+hrs"
+        del_t_aki_hr < 4 ~ "AKI in  0-4hrs",
+        del_t_aki_hr < 8 ~ "AKI in  4-8hrs",
+        del_t_aki_hr < 12 ~ "AKI in  8-12hrs",
+        del_t_aki_hr < 16 ~ "AKI in 12-16hrs",
+        del_t_aki_hr < 20 ~ "AKI in 16-20hrs",
+        del_t_aki_hr < 24 ~ "AKI in 20-24hrs",
+        del_t_aki_hr < 30 ~ "AKI in 24-30hrs",
+        TRUE ~ "AKI in 30+hrs"
       ),
     )
 
@@ -75,19 +75,19 @@ plot_cr_ch_heatmap <- function(analysis_df, save_plots) {
     scale_x_continuous(breaks = seq(0, 12, by = 2)) +
     coord_cartesian(xlim = c(0, 12), ylim = c(-25, 30), expand = FALSE) +
     facet_wrap(~heatmap) +
-    scale_fill_viridis_c("n(Cr_ch epis)\nDensity", option = "D") +
+    scale_fill_viridis_c("Density of\nCr change\nepisodes", option = "D") +
     geom_hline(yintercept = 0, colour = "white", linetype = "dotted") +
     geom_vline(xintercept = seq(0, 16, by = 4), colour = "white", linetype = "dotted") +
     geom_abline(slope = 1, intercept = 0, colour = "white", linetype = "solid") +
-    annotate("text", x = 10, y = 9, label = "1\u03BCmol/L/h", colour = "white", vjust = 1.3) +
-    geom_text(
-      data = heatmap_count,
-      aes(x = 0.2, y = -23, label = paste0("n(Admissions): ", n_admission, "\nn(Cr_ch epis): ", n_cr)),
-      colour = "white", hjust = 0, vjust = 0
-    ) +
-    ggtitle("Number of Creatinine Change Episodes which Predict AKI in X hours' time") +
-    xlab(expression("Duration of short-term Cr change epis: " * Delta * "t"["cr_ch"] * " (hours)")) +
-    ylab(expression("Change in Cr during epis: " * Delta * "cr" * " (" * mu * "mol/L)")) +
+    annotate("text", x = 10, y = 17, label = "1\u03BCmol/L/h", colour = "white", vjust = 1.3, angle = 10) +
+    # geom_text(
+    #   data = heatmap_count,
+    #   aes(x = 0.2, y = -23, label = paste0("n(Admissions): ", n_admission, "\nn(Cr_ch epis): ", n_cr)),
+    #   colour = "white", hjust = 0, vjust = 0
+    # ) +
+    ggtitle("The frequency at which small, short-term changes in creatinine predict imminent AKI") +
+    xlab(expression("Duration of short-term Cr change episode: " * Delta * "t" * " (hours)")) +
+    ylab(expression("Change in Cr during episode:     " * Delta * "cr" * " (" * mu * "mol/L)")) +
     theme(panel.spacing = unit(0.8, "lines")) +
     theme(
       plot.background = element_rect(fill = "transparent", colour = NA),
