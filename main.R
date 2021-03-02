3ed629d0225ef67ff94812549705b4960b1ed940
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-03-03 07:56:52 +1100

    Next project steps

--- main.R
+++ main.R
@@ -17,8 +17,8 @@
 # ---- Load Functions ----
 file_sources <- list.files(path = "R/", pattern = "^[0-9][0-9].*.R$", full.names = TRUE)
 
-max_num <- 10
-excl_num <- c()
+max_num <- 13
+excl_num <- c(12, 14)
 file_nums <- as.numeric(gsub(".*([0-9]{2})_[A-Za-z].*", "\\1", file_sources))
 file_sources <- file_sources[file_nums <= max_num & !(file_nums %in% excl_num)]
 rm(max_num, excl_num, file_nums)
