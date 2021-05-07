931e5a7f6633c9706d56434da6a937c688558751
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-05-07 23:12:08 +1000

    Estimation for Score

--- main.R
+++ main.R
@@ -245,3 +245,62 @@ opt_cut_optim_model <- cutpointr(
   boot_runs = 1000
 )
 boot_ci(opt_cut_optim_model, AUC, in_bag = TRUE, alpha = 0.05)
+
+
+# Score
+manual_predictor <- function(PCs_cardio, Vasopressor, Chronic_liver_disease, cr_gradient) {
+  y <- coef(multi_model$optim_model$model)["(Intercept)"] +
+    coef(multi_model$optim_model$model)["PCs_cardio"] * PCs_cardio +
+    coef(multi_model$optim_model$model)["Vasopressor"] * Vasopressor +
+    coef(multi_model$optim_model$model)["Chronic_liver_disease"] * Chronic_liver_disease +
+    coef(multi_model$optim_model$model)["cr_gradient"] * cr_gradient
+  as.numeric(1 / (1 + exp(-y)))
+}
+
+stopifnot(all.equal(
+  manual_predictor(
+    multi_model$optim_model$data$PCs_cardio,
+    multi_model$optim_model$data$Vasopressor,
+    multi_model$optim_model$data$Chronic_liver_disease,
+    multi_model$optim_model$data$cr_gradient
+  ),
+  as.numeric(multi_model$optim_model$data$predict)
+))
+
+lm_score <- lm(
+  predict ~ PCs_cardio + Vasopressor + Chronic_liver_disease + cr_gradient,
+  multi_model$optim_model$data
+)
+
+manual_predictor(
+  PCs_cardio = 0,
+  Vasopressor = 1,
+  Chronic_liver_disease = 0,
+  cr_gradient = 0
+)
+
+BrierScore(multi_model$optim_model$data$AKI_2or3, multi_model$optim_model$data$predict)
+
+brier_wrapper <- function(b, data) {
+  y <- 0 + b[1] +
+    b[2] * data$PCs_cardio +
+    b[3] * data$Vasopressor +
+    b[4] * data$Chronic_liver_disease +
+    b[5] * data$cr_gradient
+  p <- as.numeric(1 / (1 + exp(-y)))
+  BrierScore(data$AKI_2or3, p)
+}
+
+# brier_wrapper(c(-4.9068747, 1.7988061, 1.1770319, 3.6879612, 0.9126502), multi_model$optim_model$data)
+
+score_coef <- DEoptim(
+  brier_wrapper,
+  rep(-10, 5),
+  rep(10, 5),
+  DEoptim.control(NP = 320, itermax = 500, trace = 100),
+  fnMap = function(x) c(x[1], round(x[2:5], 0)),
+  data = multi_model$optim_model$data
+)
+score_coef$optim$bestval
+score_coef$optim$bestmem
+# Summary: Vasopressor is 3x more important than others
